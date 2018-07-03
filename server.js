655f87c8819a51e5fa9180030df5f4986e42f8e2
touqeeraslam <touqeeraslamkhan@gmail.com>
2018-07-03 15:07:50 +0500

    sadaasd

--- server.js
+++ server.js
@@ -32,7 +32,7 @@
     // console.log(req.body.docusignenvelopeinformation);
     // console.log(req.body.docusignenvelopeinformation.documentpdfs);
     console.log("this is the required console : ",req.body.docusignenvelopeinformation.documentpdfs);
-    console.log("this is the required console : ",req.body.docusignenvelopeinformation.documentpdfs[0]);
+    console.log("this is the required console : ",req.body.docusignenvelopeinformation.documentpdfs[0].documentpdf[0].pdfbytes);
    
     return res.send('success');
 });
